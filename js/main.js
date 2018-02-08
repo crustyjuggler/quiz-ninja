170fbd4153affde5797c6ca40ad111d22e5e8e30
“crustyjuggler” <damonscott01@gmail.com>
2018-02-08 13:24:09 -0500

    added chapter 3 changes to the quiz using array and for-of loop

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 alert("Welcome to Quiz Ninja!");
 
-const question = "What is Superman's real name?";
-const answer = prompt(question);
+const quiz = [
+	["What is Superman's real name?", "Clark Kent"],
+	["What is Wonder Woman's real name?", "Diana Prince"],
+    ["What is Batman's real name?", "Bruce Wayne"]
+];
 
-alert(`You answered ${answer}`);
+let score = 0;
+
+for (const [question, answer] of quiz) {
+	const response = prompt(question);
+	if (response === answer) {
+		alert('Correct!');
+		score++;
+	} else {
+		alert(`Wrong! The correct answer is ${answer}.`);
+	}
+}
+
+alert(`Game Over...you scored ${score} point${score !== 1 ? 's' : ''}!`);
