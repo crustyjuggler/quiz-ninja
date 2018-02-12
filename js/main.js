7676dc1f2c83bf8b43505327d14f4e6bcb8fb535
“crustyjuggler” <damonscott01@gmail.com>
2018-02-12 17:00:48 -0500

    added chapter 5 changes to the quiz using an object to namespace the functions

--- js/main.js
+++ js/main.js
@@ -1,36 +1,42 @@
 alert("Welcome to Quiz Ninja!");
 
 const quiz = [
-	["What is Superman's real name?", "Clark Kent"],
-	["What is Wonder Woman's real name?", "Diana Prince"],
-    ["What is Batman's real name?", "Bruce Wayne"]
+	{ name: "Superman", realName: "Clark Kent" },
+	{ name: "Wonder Woman", realName: "Diana Prince" },
+	{ name: "Batman", realName: "Bruce Wayne" },
 ];
 
-function start(quiz) {
-	let score = 0;
+const game = {
+	start(quiz) {
+		this.questions = [...quiz];
+		this.score = 0;
 
-	for (const [question, answer] of quiz) {
-		const response = ask(question);
-		check(response, answer);
-	}
+		for (const question of this.questions) {
+			this.question = question;
+			this.ask();
+		}
+		this.gameOver();
+	},
 
-	gameOver();
+	ask() {
+		const question = `What is ${this.question.name}'s real name?`;
+		const response = prompt(question);
+		this.check(response);
+	},
 
-	function ask(question) {
-		return prompt(question);
-	}
-
-	function check(response, answer) {
+	check(response) {
+		const answer = this.question.realName;
 		if (response === answer) {
 			alert('Correct!');
-			score++;
+			this.score++;
 		} else {
 			alert(`Wrong! The correct answer is ${answer}.`);
 		}
-	}
-	function gameOver() {
-		alert(`Game Over...you scored ${score} point${score !== 1 ? 's' : ''}!`);
+	},
+
+	gameOver() {
+		alert(`Game Over...you scored ${this.score} point${this.score !== 1 ? 's' : ''}!`);
 	}
 }
 
-start(quiz);
+game.start(quiz);
